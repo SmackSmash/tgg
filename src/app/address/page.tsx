d16751381d3655be6b31ab95d8d459742e17b681
Dan Moffat <danmoffat@gmail.com>
2025-12-30 14:47:19 +0000

    Add address page

---
+++ src/app/address/page.tsx
@@ -0,0 +1,9 @@
+import TopBanner from '../../components/top-banner';
+
+export default function Address() {
+  return (
+    <main className='px-4 py-2'>
+      <TopBanner>22.43 million households in UK could be affected</TopBanner>
+    </main>
+  );
+}
