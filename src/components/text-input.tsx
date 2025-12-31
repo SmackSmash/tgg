f80c401851af97830a04dc36729f20b243afd67b
Dan Moffat <danmoffat@gmail.com>
2025-12-31 21:21:18 +0000

    Manually pass value through to component

--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -9,6 +9,7 @@ export default function TextInput({ required, value, ...rest }: TextInputProps) 
     <>
       <input
         type='text'
+        value={value}
         {...rest}
         className='bg-input-grey min-w-0 grow rounded-sm px-1.75 py-3.25 outline-0'
       />
