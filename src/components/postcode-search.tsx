58c98df744512c54dd8f8d751fab442b14154639
Dan Moffat <danmoffat@gmail.com>
2026-01-02 15:39:41 +0000

    Convert postcode form to use react-hook-form

--- src/components/postcode-search.tsx
+++ src/components/postcode-search.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, type ChangeEvent, type FormEvent } from 'react';
+import { type FieldValues, useForm } from 'react-hook-form';
 import { useAppDispatch, useAppSelector } from '@/src/store/hooks';
 import { addPostcode, addAddress } from '@/src/store';
 import TextInput from './text-input';
@@ -9,14 +10,18 @@ import { getAddress } from '@/src/api';
 export default function PostcodeSearch() {
   const postcode = useAppSelector(({ form: { postcode } }) => postcode);
   const dispatch = useAppDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid, errors }
+  } = useForm();
   const [isPending, setIsPending] = useState(false);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     dispatch(addPostcode(e.target.value));
   };
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit = async () => {
     setIsPending(true);
     const returnedAddress = await getAddress(postcode);
     setIsPending(false);
@@ -24,17 +29,26 @@ export default function PostcodeSearch() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className='py-1'>
+    <form onSubmit={handleSubmit(onSubmit)} className='py-1'>
       <p className='pb-2'>Enter your postcode and tap &lsquo;Search&rsquo;.</p>
       <div className='flex items-center gap-4'>
-        <TextInput placeholder='Postcode' onChange={handleChange} value={postcode} />
+        <TextInput
+          placeholder='Postcode'
+          value={postcode}
+          {...register('postcode', {
+            onChange: handleChange,
+            required: 'Please enter a valid postcode',
+            minLength: { value: 5, message: 'Postcode must be at least 5 characters' }
+          })}
+        />
         <button
-          disabled={isPending || postcode.length < 5}
+          disabled={!isValid || isPending}
           className='bg-cta-red disabled:bg-cta-disabled flex cursor-pointer rounded-full px-10.25 py-1.75 text-[15px] font-semibold text-white'
         >
           Search
         </button>
       </div>
+      {errors.postcode?.message && <p>{errors.postcode.message}</p>}
     </form>
   );
 }
--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -1,26 +1,17 @@
-import { useState } from 'react';
-
 type TextInputProps = {
-  required?: boolean;
   value: string;
   [x: string]: unknown;
 };
 
-export default function TextInput({ required, value, ...rest }: TextInputProps) {
-  const [touched, setTouched] = useState(false);
-
+export default function TextInput({ value, ...rest }: TextInputProps) {
   return (
     <>
       <input
         type='text'
         value={value}
         {...rest}
-        onBlur={() => setTouched(true)}
         className='bg-input-grey min-w-0 grow rounded-xs px-1.75 py-3.25 outline-0'
       />
-      {required && touched && !value.length && (
-        <p className='text-cta-red'>This is a required field</p>
-      )}
     </>
   );
 }
