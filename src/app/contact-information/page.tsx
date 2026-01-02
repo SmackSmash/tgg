c1dd01263e0203d6c9fb746b7a4fe8c02f41ab42
Dan Moffat <danmoffat@gmail.com>
2026-01-02 18:46:27 +0000

    Add secure logo to text inputs

--- src/app/contact-information/page.tsx
+++ src/app/contact-information/page.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image';
+import CTALink from '@/src/components/cta-link';
+import TextInput from '@/src/components/text-input';
 import TopBanner from '@/src/components/top-banner';
+import SearchIcon from '@/public/search.svg';
 
 export default function ContactInformation() {
   return (
     <main className='px-4 py-2'>
       <TopBanner>Final step! Your claim is 100% no-win, no-fee!</TopBanner>
+      <form className='flex flex-col'>
+        <TextInput value={''} secure />
+        <CTALink href='/address' disabled={true}>
+          <Image src={SearchIcon} alt='Search Icon' />
+          Find My Agreements
+        </CTALink>
+      </form>
     </main>
   );
 }
--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -1,15 +1,16 @@
 type TextInputProps = {
   value: string;
+  secure?: boolean;
   [x: string]: unknown;
 };
 
-export default function TextInput({ value, ...rest }: TextInputProps) {
+export default function TextInput({ value, secure, ...rest }: TextInputProps) {
   return (
     <input
       type='text'
       value={value}
       {...rest}
-      className='bg-input-grey min-w-0 grow rounded-xs px-1.75 py-3.25 outline-0'
+      className={`bg-input-grey min-w-0 grow rounded-xs px-1.75 py-3.25 outline-0 ${secure && 'bg-[url("../../public/secure-ssl-dark.svg")] bg-size-[55px] bg-position-[95%_center] bg-no-repeat'}`}
     />
   );
 }
