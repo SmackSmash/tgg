4c0ae88845999659a87f1a2f6ae7e14cb56baeb5
Dan Moffat <danmoffat@gmail.com>
2026-01-09 16:42:57 +0000

    Update footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,13 +6,13 @@ export default function Footer() {
     <>
       <SRANotice />
       <footer className='flex gap-4 bg-black px-5 py-5'>
-        <Link href='_blank' className='text-[10px] text-white underline'>
+        <Link href='' target='_blank' className='text-[10px] text-white underline'>
           Terms & Conditions
         </Link>
-        <Link href='_blank' className='text-[10px] text-white underline'>
+        <Link href='' target='_blank' className='text-[10px] text-white underline'>
           Complaints Procedure
         </Link>
-        <Link href='_blank' className='text-[10px] text-white underline'>
+        <Link href='' target='_blank' className='text-[10px] text-white underline'>
           Privacy Policy
         </Link>
       </footer>
